d8ae95cf62c23102ceeb8746805fac5fc4ad6aac
eshapatel2005 <esha.patel5605@gmail.com>
2026-07-10 16:26:27 +0530

    Formated Document

--- models/user.model.js
+++ models/user.model.js
@@ -1,37 +1,37 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const userSchema = new mongoose.Schema({
-
-    name:{
-        type:String,
-        required:true
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
 
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
 
-    age:{
-        type:Number,
-        required:true
+    age: {
+      type: Number,
+      required: true,
     },
 
-    city:{
-        type:String,
-        required:true
+    city: {
+      type: String,
+      required: true,
     },
 
-    password:{
-        type:String,
-        required:true
-    }
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-},
-{
-    timestamps:true
-});
-
-module.exports = mongoose.model("User",userSchema);
+module.exports = mongoose.model("User", userSchema);
