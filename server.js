180e6049d2c60a63b759f73194a5d43a62f7c12d
eshapatel2005 <esha.patel5605@gmail.com>
2026-07-30 15:15:57 +0530

    Integrate upload routes into server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 
 dotenv.config();
 
@@ -11,15 +12,22 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Static Folder for Uploaded Files
+app.use("/api/uploads", express.static(path.join(__dirname, "uploads")));
+
 // User Routes
 const userRouter = require("./routes/users.routes");
 
 // Product Routes
 const productsRouter = require("./routes/products.routes");
 
+// Upload Routes
+const uploadRouter = require("./routes/upload");
+
 // Routes
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/product", productsRouter);
+app.use("/api/v1/upload", uploadRouter);
 
 const PORT = process.env.PORT || 8001;
 
