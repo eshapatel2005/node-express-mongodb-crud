be15b45c5805e1f0ca3177bd34dc031d136a2dec
eshapatel2005 <esha.patel5605@gmail.com>
2026-08-14 15:08:36 +0530

    Add user aggregation match

--- aggregation/aggregate.js
+++ aggregation/aggregate.js
@@ -1,10 +1,22 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
+const User = require("../models/user.model");
 
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => {
+  .then(async () => {
     console.log("MongoDB Connected Successfully");
+
+    const result = await User.aggregate([
+      {
+        $match: {
+          age: { $gt: 18 }
+        }
+      }
+    ]);
+
+    console.log(result);
+    mongoose.connection.close();
   })
   .catch((err) => {
     console.log("MongoDB Connection Error:", err);
